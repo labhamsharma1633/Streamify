7610cf4192ef1e06f14bc2cbc8a49fb818e85097
Labham Sharma <labham1633@gmail.com>
2026-04-17 12:48:27 +0530

    fix: remove local dependency

--- streamfy/backend/src/server.js
+++ streamfy/backend/src/server.js
@@ -12,9 +12,12 @@ import chatRoutes from "./routes/chat.route.js";
 const app=express();
 const PORT=process.env.PORT;
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true  //allow frontend to send cookies
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://streamify22.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- streamfy/frontend/src/lib/axios.js
+++ streamfy/frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" 
     ? "http://localhost:5001/api" 
-    : "https://streamify-97zt.onrender.com/api"; // <- backend live URL
+    : "https://streamify22.onrender.com/api"; // <- backend live URL
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
