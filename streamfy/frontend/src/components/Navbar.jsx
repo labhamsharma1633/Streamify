88e468458a9de54a5e911b76f2befc789803ec79
Labham Sharma <labham1633@gmail.com>
2026-04-17 14:16:14 +0530

    fix: cookie for production

--- streamfy/frontend/src/components/Navbar.jsx
+++ streamfy/frontend/src/components/Navbar.jsx
@@ -51,7 +51,10 @@ const Navbar = () => {
           </div>
 
           {/* Logout button */}
-          <button className="btn btn-ghost btn-circle" onClick={() => logoutMutation()}>
+          <button className="btn btn-ghost btn-circle" onClick={() => {
+            console.log("clicked");
+            logoutMutation();
+          }}>
             <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
           </button>
         </div>
